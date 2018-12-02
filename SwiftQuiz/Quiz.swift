4a3c2a63b0f8064c485fdcc687d42e736de9d4a9
Lucas Bighi <lucasmbighi@live.com>
2018-12-02 15:53:49 -0200

    Lógica implementada
    Agora o app é funcional

--- SwiftQuiz/Quiz.swift
+++ SwiftQuiz/Quiz.swift
@@ -9,5 +9,22 @@
 import Foundation
 
 class Quiz {
+    let question: String
+    let options: [String]
+    private let correctedAnswer: String
     
+    init(question: String, options: [String], correctedAnswer: String) {
+        self.question = question
+        self.options = options
+        self.correctedAnswer = correctedAnswer
+    }
+    
+    func validateOptions(_ index: Int) -> Bool {
+        let answer = options[index]
+        return answer == correctedAnswer
+    }
+    
+    deinit {
+        print("Liberou quiz da memória")
+    }
 }
